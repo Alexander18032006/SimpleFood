04a38240e7c93c2248c431f7ec550f8d61c97bc1
Alexander <nazdrin.s@gmail.com>
2022-07-09 14:44:25 +0300

    finished desktop

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,8 @@
 $(function () {
+	$('.comment__list').slick({
+		dots: true,
+		prevArrow: '<button class="slick-prev slick-arrow active" type="button"><svg class="slick-arrow__svg"><use xlink:href="images/sprite.svg#arrow"></use></svg></button>',
+		nextArrow: '<button class="slick-next slick-arrow" type="button"><svg class="slick-arrow__svg"><use xlink:href="images/sprite.svg#arrow"></use></svg></button>',
+	})
 	var mixer = mixitup('.popular__content');
 });
