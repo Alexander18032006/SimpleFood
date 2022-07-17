a207a1139fb208b4c6349960a5df2c38d1933e73
Alexander <nazdrin.s@gmail.com>
2022-07-17 09:12:57 +0300

    fixed

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,10 @@
 $(function () {
 	$('.comment__list').slick({
 		dots: true,
-		prevArrow: '<button class="slick-prev slick-arrow active" type="button"><svg class="slick-arrow__svg"><use xlink:href="images/sprite.svg#arrow"></use></svg><span class="sr-only">Слайдер влево</span></button>',
-		nextArrow: '<button class="slick-next slick-arrow" type="button"><svg class="slick-arrow__svg"><use xlink:href="images/sprite.svg#arrow"></use><span class="sr-only">Слайдер вправо</span></svg></button>',
+		appendDots: $('.comment__dots'),
+		appendArrows: $('.comment__arrows'),
+		prevArrow: '<button class="comment__arrow-prev comment__arrow active" type="button"><svg class="comment__svg"><use xlink:href="images/sprite.svg#arrow"></use></svg><span class="sr-only">Слайдер влево</span></button>',
+		nextArrow: '<button class="comment__arrow-next comment__arrow" type="button"><svg class="comment__svg"><use xlink:href="images/sprite.svg#arrow"></use><span class="sr-only">Слайдер вправо</span></svg></button>',
 	})
 	var mixer = mixitup('.popular__content');
 });
