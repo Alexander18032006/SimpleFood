126b34b8e573719b7e3e51b74fc61c37a3cd9603
Alexander <nazdrin.s@gmail.com>
2022-07-31 10:08:38 +0300

    fix

--- app/js/main.js
+++ app/js/main.js
@@ -6,6 +6,14 @@ $(function () {
 		prevArrow: '<button class="comment__arrow-prev comment__arrow active" type="button"><svg class="comment__svg"><use xlink:href="images/sprite.svg#arrow"></use></svg><span class="sr-only">Слайдер влево</span></button>',
 		nextArrow: '<button class="comment__arrow-next comment__arrow" type="button"><svg class="comment__svg"><use xlink:href="images/sprite.svg#arrow"></use><span class="sr-only">Слайдер вправо</span></svg></button>',
 	});
+	if (window.matchMedia("(max-width: 768px)").matches) {
+		$('.filter').slick({
+			arrows: false,
+			slidesToShow: 2,
+			slidesToScroll: 3,
+		}
+		);
+	}
 	$('.burger').on('click', function () {
 		$('.mobile-menu').addClass('active');
 		$('body').addClass('active');
