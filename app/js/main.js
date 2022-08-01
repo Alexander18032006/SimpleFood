2c71d7f091df6882c79a42449b0e982967a4b9c0
Alexander <nazdrin.s@gmail.com>
2022-08-01 11:17:33 +0300

    fixed

--- app/js/main.js
+++ app/js/main.js
@@ -5,6 +5,15 @@ $(function () {
 		appendArrows: $('.comment__arrows'),
 		prevArrow: '<button class="comment__arrow-prev comment__arrow active" type="button"><svg class="comment__svg"><use xlink:href="images/sprite.svg#arrow"></use></svg><span class="sr-only">Слайдер влево</span></button>',
 		nextArrow: '<button class="comment__arrow-next comment__arrow" type="button"><svg class="comment__svg"><use xlink:href="images/sprite.svg#arrow"></use><span class="sr-only">Слайдер вправо</span></svg></button>',
+		responsive: [
+			{
+				breakpoint: 769,
+				settings: {
+					dots: false,
+					appendArrows: $('.comment__list'),
+				}
+			},
+		]
 	});
 	if (window.matchMedia("(max-width: 768px)").matches) {
 		$('.filter').slick({
